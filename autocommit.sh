5f827295b761cc4ca3cf200329cf59591acbb211
yevgenyt <133467239+yevgenyt@users.noreply.github.com>
2024-01-18 20:49:30 -0500

    pass arbitrary commit message from UI
    we also need to change config_backup.cfg

--- autocommit.sh
+++ autocommit.sh
@@ -75,10 +75,10 @@ push_config(){
   cd $config_folder
   git pull origin $branch --no-rebase
   git add .
-  current_date=$(date +"%Y-%m-%d %T")
-  git commit -m "Autocommit from $current_date" -m "$m1" -m "$m2" -m "$m3" -m "$m4"
+  git commit -m "$*"
   git push origin $branch
 }
 
 grab_version
-push_config
+# TODO need to figure out handling of quotes in the user input
+push_config "$*"
